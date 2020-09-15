4e076a1b2a0a886a0c5bececeff88e6d4afd3e10
Joseph Garrison <braisengeek@gmail.com>
2020-09-15 11:06:21 -0500

    implemented form event handler

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-function validateFormValues(values){
-    console.log(values);
-    return true;
+window.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('signup-form') .addEventListener('submit', validateFormValues);
+});
+
+
+function validateFormValues(event){
+    let form = document.getElementById('signup-form');
+    let username = form.elements['name'].value;
+    let useremail= form.elements['email'].value;
+    let userpw = form.elements['password'].value;
+
+    var user = {
+        name: username,
+        email: useremail,
+        password: userpw
+    }
+    console.log(user);
+    event.preventDefault();
 }
+
+
+
+
+
+// var user = {
+//     firstname: 'Joe',
+//     lastname: 'Garrison',
+//     email: 'joe@gmail.com',
+//     fullname: function(){
+//         return this.firstname+ " " + this.lastname;
+//     }
+
+// }
